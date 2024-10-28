874333af1a801603c201ebf55afbc89833273a26
pinyili2 <pinyili2@illinois.edu>
2024-10-28 11:06:43 -0500

    make extforce and torque link to rbtype

--- src/RigidBodyType.h
+++ src/RigidBodyType.h
@@ -24,6 +24,7 @@ class RigidBodyType {
 public:
 RigidBodyType(const String& name = "", const Configuration* conf = NULL ) :
 	name(name), conf(conf), num(0),
+	constantTorque(Vector3(0.f)),constantForce(Vector3(0.f)),
 		reservoir(NULL), mass(1.0f), inertia(), transDamping(),
 		rotDamping(), initPos(), initRot(Matrix3(1.0f)), initMomentum(Vector3(0.f)), initAngularMomentum(Vector3(0.f)),
 		numPotGrids(0), numDenGrids(0), numPmfs(0), numParticles(NULL) { }
@@ -77,7 +78,9 @@ public:
 	Vector3 rotDamping;
 	Vector3 transForceCoeff;
 	Vector3 rotTorqueCoeff;
-
+        Vector3 constantTorque;
+        Vector3 constantForce;
+        Vector3 vectorTorque;
 	Vector3 initPos;	
 	Matrix3 initRot;
         Vector3 initMomentum;
