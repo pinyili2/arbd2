436ec30ec81553be66c81db33271b6d5dc53798d
pinyili2 <pinyili2@illinois.edu>
2025-01-26 21:58:02 -0600

    Revert "remove a line"
    This reverts commit b46b2a001551e3bdbc47049066b504bf4ecde118.

--- src/Proxy.h
+++ src/Proxy.h
@@ -308,6 +308,7 @@ struct Proxy {
 	    }
 	    break;
 	case Resource::GPU:
+	    case Resource::GPU:
 	#ifdef __CUDACC__
 	    if (location.is_local()) {
             return std::async(std::launch::async, [this, memberFunc, args...] {
