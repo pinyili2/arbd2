5968c6f027f6336ca87da88895034c01eb50afda
pinyili2 <pinyili2@illinois.edu>
2025-05-22 15:24:25 -0500

    update to variadic

--- src/ARBDException.cpp
+++ src/ARBDException.cpp
@@ -1,39 +1,6 @@
 #include "ARBDException.h"
-#include <limits>    
-#include <vector>  
-#include <sstream>  
-#include <cstdarg>  
-#include <cstdio>    
-#include <source_location> 
 
 namespace ARBD {
-
-std::string Exception::sformat(const std::string &fmt_str, va_list &ap) {
-    int size = 512;
-    const int max_size = 32768; // Practical limit
-    std::string str;
-    
-    while(size < max_size) {
-        str.resize(size);
-        va_list ap_copy;
-        va_copy(ap_copy, ap);
-        // Use &str[0] in C++11 and later as string data is contiguous
-        int n = vsnprintf(&str[0], size, fmt_str.c_str(), ap_copy);
-        va_end(ap_copy);
-        
-        if (n < 0) {
-            // vsnprintf error
-            return "[sformat error]";
-        }
-        if (n < size) { // Content fit
-            str.resize(n);
-            return str;
-        }
-        // Buffer too small, new size is n + 1 (for null terminator)
-        size = n + 1;  
-    }
-    return "[Error: formatted string too long in sformat]";
-}
 
 std::string Exception::type_to_str(ExceptionType type) {
     switch (type) {
@@ -49,30 +16,4 @@ std::string Exception::type_to_str(ExceptionType type) {
     }
 }
 
-Exception::Exception(
-    ExceptionType type,
-    const char* message_format, 
-    const std::source_location& location,
-    ...
-) : _error_message() { 
-    std::ostringstream location_info;
-    location_info << location.file_name()
-                  << "(" << location.line()
-                  << ") '" << location.function_name() << "'";
-
-    std::string user_message;
-    if (message_format) {
-        va_list ap;
-        va_start(ap, location); 
-        user_message = sformat(message_format, ap);
-        va_end(ap);
-    }
-    
-    _error_message = type_to_str(type) + ": " + user_message + " [" + location_info.str() + "]";
-}
-
-const char* Exception::what() const noexcept {
-    return _error_message.c_str();
-}
-
-} // namespace arbd
+} 
--- src/ARBDException.h
+++ src/ARBDException.h
@@ -1,10 +1,11 @@
 #pragma once
 
 #include <string>
-#include <cstdarg>
 #include <exception>
-#include <source_location> // C++20 requirement
-#include "ARBDHeaders.h"
+#include <source_location>
+#include <sstream>
+#include <iostream>
+#include <format> // C++20 feature
 
 namespace ARBD {
 enum class ExceptionType { 
@@ -18,23 +19,87 @@ enum class ExceptionType {
 };
 
 class Exception : public std::exception {
-  private:
-    std::string _error_message; // Changed from _error for clarity
-    std::string sformat(const std::string &fmt, va_list &ap);
+private:
+    std::string _error_message;
+    ExceptionType _type;
+    std::source_location _location;
+    
     static std::string type_to_str(ExceptionType type);
 
-  public:
-
+public:
+    // Modern variadic template constructor
+    template<typename... Args>
     Exception(
         ExceptionType type,
-        const char* message_format, 
-        const std::source_location& location = std::source_location::current(),
-        ...
-    );
+        const std::source_location& location,
+        std::format_string<Args...> fmt,
+        Args&&... args
+    ) : _type(type), _location(location) {
+        std::ostringstream oss;
+        oss << type_to_str(type) << ": ";
+        
+        try {
+            oss << std::format(fmt, std::forward<Args>(args)...);
+        } catch (const std::format_error& e) {
+            oss << "[Format error: " << e.what() << "]";
+        }
+        
+        oss << " [" << _location.file_name()
+            << "(" << _location.line()
+            << ") '" << _location.function_name() << "']";
+            
+        _error_message = oss.str();
+    }
+    
+    // Convenience constructor for simple messages
+    Exception(
+        ExceptionType type,
+        std::string_view message,
+        const std::source_location& location = std::source_location::current()
+    ) : _type(type), _location(location) {
+        std::ostringstream oss;
+        oss << type_to_str(type) << ": " << message << " [" << _location.file_name()
+            << "(" << _location.line() << ") '" << _location.function_name() << "']";
+        _error_message = oss.str();
+    }
 
-    virtual const char* what() const noexcept override; 
+    virtual const char* what() const noexcept override { 
+        return _error_message.c_str(); 
+    }
+    
+    ExceptionType type() const noexcept { return _type; }
+    const std::source_location& where() const noexcept { return _location; }
 };
 
+// Factory functions for common exceptions
+template<typename... Args>
+[[noreturn]] inline void throw_not_implemented(
+    std::format_string<Args...> fmt, 
+    Args&&... args,
+    const std::source_location& loc = std::source_location::current()
+) {
+    throw Exception(ExceptionType::NotImplementedError, loc, fmt, std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+[[noreturn]] inline void throw_value_error(
+    std::format_string<Args...> fmt, 
+    Args&&... args,
+    const std::source_location& loc = std::source_location::current()
+) {
+    throw Exception(ExceptionType::ValueError, loc, fmt, std::forward<Args>(args)...);
+}
+
+template<typename... Args>
+[[noreturn]] inline void throw_cuda_error(
+    std::format_string<Args...> fmt, 
+    Args&&... args,
+    const std::source_location& loc = std::source_location::current()
+) {
+    throw Exception(ExceptionType::CUDARuntimeError, loc, fmt, std::forward<Args>(args)...);
+}
+
 } // namespace ARBD
-#define ARBD_Exception(type, fmt_str, ...) \
-    throw ARBD::Exception(type, fmt_str, std::source_location::current(), ##__VA_ARGS__)
+
+#define ARBD_Exception(type, ...) \
+    throw ARBD::Exception(type, std::source_location::current(), __VA_ARGS__)
