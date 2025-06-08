8340a763fd731fc49a179120c0ae9a369ddc2651
pinyili2 <113645788+pinyili2@users.noreply.github.com>
2025-06-07 22:41:51 -0500

    update from using Vector3 to ARBD::CUDA

--- src/Patch/GPU/CUDA.h
+++ src/Patch/GPU/CUDA.h
@@ -1,6 +1,8 @@
 #pragma once
-#include "Backend/GPU/GPUManager.h"
-#include "ParticlePatch.h"
+#ifdef USE_CUDA
+#include "Backend/CUDA/CUDAManager.h"
+#include "Types/Types.h"
+#include "../ParticlePatch.h"
 
 namespace ARBD {
 
@@ -16,15 +18,18 @@ private:
     
     // Device arrays
     size_t* groupSiteData_d;
-    Vector3* pos_force_d;
-    Vector3* momentum_d;
-    Vector3* rb_pos_d;
-    Matrix3* rb_orient_d;
-    Vector3* rb_mom_d;
-    Vector3* rb_ang_mom_d;
+    //Vector3* pos_force_d;
+    //Vector3* momentum_d;
+    ARBD::CUDA::DeviceMemory<Vector3> pos_force_d;
+    ARBD::CUDA::DeviceMemory<Vector3> momentum_d;
+    ARBD::CUDA::DeviceMemory<Vector3> rb_pos_d;
+    ARBD::CUDA::DeviceMemory<Matrix3> rb_orient_d;
+    ARBD::CUDA::DeviceMemory<Vector3> rb_mom_d;
+    ARBD::CUDA::DeviceMemory<Vector3> rb_ang_mom_d;
     size_t* type_d;
     size_t* rb_type_d;
 
 };
 
 } // namespace ARBD
+#endif
